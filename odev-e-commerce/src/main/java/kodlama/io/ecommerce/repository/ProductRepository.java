0635966a0977b203eb3786bc5555fef1a454380a
eminezubeyde <113181394+eminezubeyde@users.noreply.github.com>
2023-04-03 11:47:10 +0300

    update

--- odev-e-commerce/src/main/java/kodlama/io/ecommerce/repository/ProductRepository.java
+++ odev-e-commerce/src/main/java/kodlama/io/ecommerce/repository/ProductRepository.java
@@ -2,8 +2,10 @@ package kodlama.io.ecommerce.repository;
 
 import kodlama.io.ecommerce.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ProductRepository extends JpaRepository<Product,Integer> {
 
-
+        @Query(nativeQuery = true,value = "Select * from product where id=?1")
+        Product getById(int id);//gönderilen id ye göre product döner
 }
