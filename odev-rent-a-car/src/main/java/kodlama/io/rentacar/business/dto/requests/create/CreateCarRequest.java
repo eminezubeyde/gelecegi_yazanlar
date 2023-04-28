2ff4f7ec2295e5778f90dee6ce372267b73b1659
eminezubeyde <113181394+eminezubeyde@users.noreply.github.com>
2023-04-28 20:01:47 +0300

    update

--- odev-rent-a-car/src/main/java/kodlama/io/rentacar/business/dto/requests/create/CreateCarRequest.java
+++ odev-rent-a-car/src/main/java/kodlama/io/rentacar/business/dto/requests/create/CreateCarRequest.java
@@ -1,5 +1,10 @@
 package kodlama.io.rentacar.business.dto.requests.create;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Pattern;
+import kodlama.io.rentacar.common.constants.Messages;
+import kodlama.io.rentacar.common.constants.Regex;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,11 +15,13 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class CreateCarRequest {
-
-    private int modelYear;
-    private String plate;
-    private double dailyPrice;
+    @Min(0)
     private int modelId;
-    private int brandId;
-
+    @Min(1996)
+    private int modelYear;
+    @Pattern(regexp = Regex.Plate, message = Messages.Car.PlateNotValid)
+    private String plate;
+    @Min(1)
+    @Max(100000)
+    private double dailyPrice;
 }
--- odev-rent-a-car/src/main/java/kodlama/io/rentacar/business/dto/requests/create/CreatePaymentRequest.java
+++ odev-rent-a-car/src/main/java/kodlama/io/rentacar/business/dto/requests/create/CreatePaymentRequest.java
@@ -1,5 +1,7 @@
 package kodlama.io.rentacar.business.dto.requests.create;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import kodlama.io.rentacar.business.dto.requests.PaymentRequest;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -11,7 +13,8 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CreatePaymentRequest extends PaymentRequest {
-
+    @NotNull
+    @Min(value = 1)
     private double balance;
 
 }
