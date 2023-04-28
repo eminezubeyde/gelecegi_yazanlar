fe1c95e05219450886fdfb0e01dd5c5bd8b03bec
eminezubeyde <113181394+eminezubeyde@users.noreply.github.com>
2023-04-28 18:07:47 +0300

    business rules created

--- odev-rent-a-car/src/main/java/kodlama/io/rentacar/business/rules/RentalBusinessRules.java
+++ odev-rent-a-car/src/main/java/kodlama/io/rentacar/business/rules/RentalBusinessRules.java
@@ -1,24 +1,29 @@
 package kodlama.io.rentacar.business.rules;
 
+import kodlama.io.rentacar.common.constants.Messages;
+import kodlama.io.rentacar.core.exceptions.BusinessException;
 import kodlama.io.rentacar.entities.enums.State;
 import kodlama.io.rentacar.repository.RentalRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
+@Service
 @AllArgsConstructor
-@Component
 public class RentalBusinessRules {
     private final RentalRepository repository;
+
     public void checkIfRentalExists(int id) {
         if (!repository.existsById(id)) {
-            throw new RuntimeException("Kiralama bilgisine ulaşılamadı.");
+            throw new BusinessException(Messages.Rental.NotExists);
         }
     }
+
     public void checkIfCarAvailable(State state) {
-        if (state.equals(State.AVAILABLE)) {
-            throw new RuntimeException("Araç müsait değil.");
+        if (!state.equals(State.AVAILABLE)) {
+            throw new BusinessException(Messages.Car.NotAvailable);
         }
     }
+}
 
 
 }
